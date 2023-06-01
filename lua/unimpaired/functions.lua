2004be36af347f602b9a80460da68aee0634ee09
Arne Van Maele <arne.vanmaele@nexuzhealth.com>
2023-06-01 22:52:57 +0200

    fix: Removing shortcuts I don't use

--- lua/unimpaired/functions.lua
+++ lua/unimpaired/functions.lua
@@ -5,379 +5,73 @@ local M = {}
 -- the mapping while preserving the [count] when called as last statement inside
 -- the custom mapping
 local restore_dot_repetition = function(count)
-    count = count or ''
-    local callback = vim.go.operatorfunc
-    vim.go.operatorfunc = ''
-    vim.cmd('silent! normal ' .. count .. 'g@l')
-    vim.go.operatorfunc = callback
-end
-
-M.previous = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'previous')
-end
-
-M.next = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'next')
-end
-
-M.first = function()
-    vim.cmd('first')
-end
-
-M.last = function()
-    vim.cmd('last')
-end
-
-M.bprevious = function()
-    vim.cmd(vim.v.count1 .. 'bprevious')
-end
-
-M.bnext = function()
-    vim.cmd(vim.v.count1 .. 'bnext')
-end
-
-M.bfirst = function()
-    vim.cmd('bfirst')
-end
-
-M.blast = function()
-    vim.cmd('blast')
-end
-
-M.lprevious = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'lprevious')
-    vim.cmd.normal('zv')
-end
-
-M.lnext = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'lnext')
-    vim.cmd.normal('zv')
-end
-
-M.lfirst = function()
-    vim.cmd('lfirst')
-end
-
-M.llast = function()
-    vim.cmd('llast')
-end
-
-M.lpfile = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'lpfile')
-end
-
-M.lnfile = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'lnfile')
+	count = count or ""
+	local callback = vim.go.operatorfunc
+	vim.go.operatorfunc = ""
+	vim.cmd("silent! normal " .. count .. "g@l")
+	vim.go.operatorfunc = callback
 end
 
 M.cprevious = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'cprevious')
-    vim.cmd.normal('zv')
+	vim.cmd("silent! " .. vim.v.count1 .. "cprevious")
+	vim.cmd.normal("zv")
 end
 
 M.cnext = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'cnext')
-    vim.cmd.normal('zv')
+	vim.cmd("silent! " .. vim.v.count1 .. "cnext")
+	vim.cmd.normal("zv")
 end
 
 M.cfirst = function()
-    vim.cmd('cfirst')
+	vim.cmd("cfirst")
 end
 
 M.clast = function()
-    vim.cmd('clast')
-end
-
-M.cpfile = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'cpfile')
-end
-
-M.cnfile = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'cnfile')
-end
-
-M.tprevious = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'tprevious')
-end
-
-M.tnext = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'tnext')
-end
-
-M.tfirst = function()
-    vim.cmd('tfirst')
-end
-
-M.tlast = function()
-    vim.cmd('tlast')
+	vim.cmd("clast")
 end
 
 M.ptprevious = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'ptprevious')
+	vim.cmd("silent! " .. vim.v.count1 .. "ptprevious")
 end
 
 M.ptnext = function()
-    vim.cmd('silent! ' .. vim.v.count1 .. 'ptnext')
+	vim.cmd("silent! " .. vim.v.count1 .. "ptnext")
 end
 
-local get_current_wininfo = function()
-    return vim.fn.getwininfo(vim.fn.win_getid())[1]
+M.enable_hlsearch = function()
+	vim.o.hlsearch = true
 end
 
-local get_files = function(dir)
-    local entries = vim.fn.split(vim.fn.glob(dir .. '/*'), '\n')
-    local files = {}
-    for _, entry in pairs(entries) do
-        if vim.fn.isdirectory(entry) ~= 1 then
-            table.insert(files, vim.fn.fnamemodify(entry, ':t'))
-        end
-    end
-    if vim.tbl_isempty(files) then return else return files end
+M.disable_hlsearch = function()
+	vim.o.hlsearch = false
 end
 
-local file_by_offset = function(offset)
-    local dir = vim.fn.expand('%:p:h')
-    local files = get_files(dir)
-    if not files then return end
-    local current = vim.fn.expand('%:t')
-    if current == '' then
-        if offset < 0 then return dir .. '/' .. files[1]
-        else return dir .. '/' .. files[#files] end
-    else
-        local index = vim.fn.index(files, current) + 1
-        if index == 0 then return end
-        index = index + offset
-        if index < 1 then index = 1
-        elseif index > #files then index = #files end
-        return dir .. '/' .. files[index]
-    end
+M.toggle_hlsearch = function()
+	vim.o.hlsearch = not vim.o.hlsearch
 end
 
-M.previous_file = function()
-    local wininfo = get_current_wininfo()
-    if wininfo.loclist == 1 then
-        vim.cmd('silent! lolder ' .. vim.v.count1)
-    elseif wininfo.quickfix == 1 then
-        vim.cmd('silent! colder ' .. vim.v.count1)
-    else
-        local file = file_by_offset(-vim.v.count1)
-        if file then
-            vim.cmd('edit ' .. file)
-        end
-    end
+M.enable_spell = function()
+	vim.o.spell = true
 end
 
-M.next_file = function()
-    local wininfo = get_current_wininfo()
-    if wininfo.loclist == 1 then
-        vim.cmd('silent! lnewer ' .. vim.v.count1)
-    elseif wininfo.quickfix == 1 then
-        vim.cmd('silent! cnewer ' .. vim.v.count1 )
-    else
-        local file = file_by_offset(vim.v.count1)
-        if file then
-            vim.cmd('edit ' .. file)
-        end
-    end
+M.disable_spell = function()
+	vim.o.spell = false
 end
 
-M.blank_above = function()
-    vim.cmd("put! =repeat(nr2char(10), v:count1)|silent ']+")
+M.toggle_spell = function()
+	vim.o.spell = not vim.o.spell
 end
 
-M.blank_below = function()
-    vim.cmd("put =repeat(nr2char(10), v:count1)|silent '[-")
+M.enable_wrap = function()
+	vim.o.wrap = true
 end
 
-M.exchange_above = function()
-    local count = vim.v.count1
-    vim.cmd('silent! move --' .. count)
-    vim.cmd.normal('==')
-    restore_dot_repetition(count)
+M.disable_wrap = function()
+	vim.o.wrap = false
 end
 
-M.exchange_below = function()
-    local count = vim.v.count1
-    vim.cmd('silent! move +' .. count)
-    vim.cmd.normal('==')
-    restore_dot_repetition(count)
+M.toggle_wrap = function()
+	vim.o.wrap = not vim.o.wrap
 end
-
-M.exchange_section_above = function()
-    local count = vim.v.count1
-    vim.cmd("silent! '<,'>move '<--" .. count)
-    vim.cmd.normal('gv=')
-    restore_dot_repetition(count)
-end
-
-M.exchange_section_below = function()
-    local count = vim.v.count1
-    vim.cmd("silent! '<,'>move '>+" .. count)
-    vim.cmd.normal('gv=')
-    restore_dot_repetition(count)
-end
-
-M.enable_background = function()
-    vim.o.background = 'light'
-end
-
-M.disable_background = function()
-    vim.o.background = 'dark'
-end
-
-M.toggle_background = function()
-    if vim.o.background == 'dark' then M.enable_background()
-    else M.disable_background() end
-end
-
--- The plugin reticle.nvim has an option to always show the cursorline
--- number, even when the cursorline itself is not enabled. This requires the
--- cursorline setting to remain switched on. If reticle is installed, we use
--- it's utility functions to change the cursorline
-M.enable_cursorline = function()
-    local loaded, reticle = pcall(require, 'reticle')
-    if loaded then reticle.enable_cursorline()
-    else vim.o.cursorline = true end
-end
-
-M.disable_cursorline = function()
-    local loaded, reticle = pcall(require, 'reticle')
-    if loaded then reticle.disable_cursorline()
-    else vim.o.cursorline = false end
-end
-
-M.toggle_cursorline = function()
-    local loaded, reticle = pcall(require, 'reticle')
-    if loaded then reticle.toggle_cursorline()
-    else vim.o.cursorline = not vim.o.cursorline end
-end
-
-M.enable_diff = function() vim.cmd('diffthis') end
-
-M.disable_diff = function() vim.cmd('diffoff') end
-
-M.toggle_diff = function()
-    if vim.o.diff then M.disable_diff()
-    else M.enable_diff() end
-end
-
-M.enable_hlsearch = function() vim.o.hlsearch = true end
-
-M.disable_hlsearch = function() vim.o.hlsearch = false end
-
-M.toggle_hlsearch = function() vim.o.hlsearch = not vim.o.hlsearch end
-
-M.enable_ignorecase = function() vim.o.ignorecase = true end
-
-M.disable_ignorecase = function() vim.o.ignorecase = false end
-
-M.toggle_ignorecase = function() vim.o.ignorecase = not vim.o.ignorecase end
-
-M.enable_list = function() vim.o.list = true end
-
-M.disable_list = function() vim.o.list = false end
-
-M.toggle_list = function() vim.o.list = not vim.o.list end
-
-M.enable_number = function() vim.o.number = true end
-
-M.disable_number = function() vim.o.number = false end
-
-M.toggle_number = function() vim.o.number = not vim.o.number end
-
-M.enable_relativenumber = function() vim.o.relativenumber = true end
-
-M.disable_relativenumber = function() vim.o.relativenumber = false end
-
-M.toggle_relativenumber = function() vim.o.relativenumber = not vim.o.relativenumber end
-
-M.enable_spell = function() vim.o.spell = true end
-
-M.disable_spell = function() vim.o.spell = false end
-
-M.toggle_spell = function() vim.o.spell = not vim.o.spell end
-
-M.enable_colorcolumn = function() vim.o.colorcolumn = '+1' end
-
-M.disable_colorcolumn = function() vim.o.colorcolumn = '' end
-
-M.toggle_colorcolumn = function()
-    if vim.o.colorcolumn == '' then M.enable_colorcolumn()
-    else M.disable_colorcolumn() end
-end
-
-M.enable_cursorcolumn = function() vim.o.cursorcolumn = true end
-
-M.disable_cursorcolumn = function() vim.o.cursorcolumn = false end
-
-M.toggle_cursorcolumn = function() vim.o.cursorcolumn = not vim.o.cursorcolumn end
-
-M.enable_virtualedit = function() vim.o.virtualedit = 'all' end
-
-M.disable_virtualedit = function() vim.o.virtualedit = '' end
-
-M.toggle_virtualedit = function()
-    if vim.o.virtualedit == '' then M.enable_virtualedit()
-    else M.disable_virtualedit() end
-end
-
-M.enable_wrap = function() vim.o.wrap = true end
-
-M.disable_wrap = function() vim.o.wrap = false end
-
-M.toggle_wrap = function() vim.o.wrap = not vim.o.wrap end
-
-M.enable_cursorcross = function()
-    M.enable_cursorline()
-    M.enable_cursorcolumn()
-end
-
-M.disable_cursorcross = function()
-    M.disable_cursorline()
-    M.disable_cursorcolumn()
-end
-
-M.toggle_cursorcross = function()
-    local cursorline = vim.o.cursorline
-    local loaded, reticle = pcall(require, 'reticle')
-    if loaded then cursorline = reticle.is_cursorline() end
-    if cursorline and vim.o.cursorcolumn then M.disable_cursorcross()
-    else M.enable_cursorcross() end
-end
-
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
-local state = {
-    paste = nil,
-    mouse = nil,
-}
-
-local setup_paste = function()
-    state.paste = vim.o.paste
-    state.mouse = vim.o.mouse
-    vim.o.paste = true
-    vim.o.mouse = ''
-    local group = augroup('unimpaired_restore_paste', { clear = true })
-    autocmd('InsertLeave', {
-        callback = function()
-            vim.o.paste = state.paste
-            vim.o.mouse = state.mouse
-            vim.cmd('autocmd! unimpaired_restore_paste')
-        end,
-        group = group
-    })
-end
-
-M.paste_above = function()
-    setup_paste()
-    return 'O'
-end
-
-M.paste_below = function()
-    setup_paste()
-    return 'o'
-end
-
 
 return M
